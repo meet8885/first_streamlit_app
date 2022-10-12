10e184b6a4df9290596089dea73ce7577255b1f1
meet8885 <114903696+meet8885@users.noreply.github.com>
2022-10-12 16:03:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,18 +22,16 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-
- 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+  streamlit.error("Please select a fruit to get information")
+ else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+except URLerror as e:
+ streamlit.error()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -45,6 +43,9 @@ streamlit.dataframe(my_data_row)
 add_my_fruit = streamlit.text_input('Fruit name')
 streamlit.write('Thanks for adding', add_my_fruit)
 
+
+
+ 
 
 
 
